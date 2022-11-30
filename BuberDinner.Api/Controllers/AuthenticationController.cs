5877fce594b741d7aea667a7224ccb70c0966989
Mustafa Masvi <mustafamasvi786@gmail.com>
2022-11-30 12:29:19 +0530

    Add Mapster

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,10 +1,9 @@
 using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using ErrorOr;
 using MediatR;
 using BuberDinner.Application.Authentication.Commands;
-using BuberDinner.Application.Authentication.Common;
 using BuberDinner.Application.Authentication.Queries;
+using MapsterMapper;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -12,43 +11,34 @@ namespace BuberDinner.Api.Controllers;
 public class AuthenticationController : ApiController
 {
     private readonly IMediator _mediator;
-    public AuthenticationController(IMediator mediator)
+    private readonly IMapper _mapper;
+    public AuthenticationController(IMediator mediator, IMapper mapper)
     {
         _mediator = mediator;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
-        var command = new RegisterCommand(request.FirstName, request.LastName, request.Email, request.Password);
+        var command =   _mapper.Map<RegisterCommand>(request);
         var authResult = await _mediator.Send(command);
 
         return authResult.Match(
-            authResult => Ok(MapAuthResult(authResult)),
+            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
             errors => Problem(errors)
-        );
-    }
-
-    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
-    {
-        return new AuthenticationResponse(
-            authResult.User.Id,
-            authResult.User.FirstName,
-            authResult.User.LastName,
-            authResult.User.Email,
-            authResult.Token
         );
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {   
-        var query = new LoginQuery(request.Email, request.Password);
+        var query = _mapper.Map<LoginQuery>(request);
 
         var authResult  = await _mediator.Send(query);
 
         return authResult.Match(
-            authResult => Ok(MapAuthResult(authResult)),
+            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
             errors => Problem(errors)
         );
     }
---
+++ BuberDinner.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using BuberDinner.Api.Common.Errors;
+using BuberDinner.Api.Common.Mappings;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace BuberDinner.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+            services.AddControllers(); 
+            services.AddSingleton<ProblemDetailsFactory, BubberDinnerProblemDetailsFactory>();
+            services.AddMapping();
+            return services;
+    }
+}
